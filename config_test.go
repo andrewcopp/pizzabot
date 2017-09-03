539dddc4831288ecfe2d7dad73e49153b4ab9a17
Andrew Copp <andrew.w.copp@gmail.com>
2017-09-03 12:09:36 -0400

    Tests Around Config Object

--- config_test.go
+++ config_test.go
@@ -1,1 +1,35 @@
 package pizzabot
+
+import "testing"
+
+func TestParse(t *testing.T) {
+	cases := []struct {
+		in      []string
+		success bool
+	}{
+		{[]string{"exec_path"}, false},
+		{[]string{"exec_path", "5x5 (1, 3) (4, 4)"}, true},
+		{[]string{"exec_path", "5X5 (1, 3) (4, 4)"}, false},
+		{[]string{"exec_path", "5x5 (1 3) (4 4)"}, false},
+		{[]string{"exec_path", "5x5 (1,3) (4,4)"}, false},
+		{[]string{"exec_path", "5x5 [1, 3] [4, 4]"}, false},
+		{[]string{"exec_path", "5x5 (1, 3, 0) (4, 4, 0)"}, false},
+		{[]string{"exec_path", "5x5x1 (1, 3) (4, 4)"}, false},
+		{[]string{"exec_path", "Ax5 (1, 3) (4, 4)"}, false},
+		{[]string{"exec_path", "5xB (1, 3) (4, 4)"}, false},
+		{[]string{"exec_path", "5x5 (C, 3) (D, 4)"}, false},
+		{[]string{"exec_path", "5x5 (1, E) (4, F)"}, false},
+	}
+
+	for _, c := range cases {
+		config := Config{}
+		err := config.Parse(c.in)
+		if err != nil && c.success == true {
+			t.Error("Case should have passed.")
+		}
+
+		if err == nil && c.success == false {
+			t.Error("Case should have failed.")
+		}
+	}
+}

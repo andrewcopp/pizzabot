2668226a97e6a835965b8bcc39a7c498982f250b
Andrew Copp <andrew.w.copp@gmail.com>
2017-09-03 12:29:05 -0400

    100% Test Coverage

--- bot_test.go
+++ bot_test.go
@@ -1,1 +1,42 @@
 package pizzabot
+
+import (
+	"testing"
+
+	"github.com/andrewcopp/geometry"
+)
+
+func TestSimpleSolve(t *testing.T) {
+	size := geometry.NewSize(2, 2)
+	pointA := geometry.NewPoint(1, 1)
+	pointB := geometry.NewPoint(0, 0)
+	cases := []struct {
+		in  *Config
+		out string
+	}{
+		{&Config{Size: size, Points: []*geometry.Point{pointA}}, "END"},
+		{&Config{Size: size, Points: []*geometry.Point{pointB}}, "D"},
+		{&Config{Size: size, Points: []*geometry.Point{pointA, pointB}}, "ENDWSD"},
+	}
+
+	for _, c := range cases {
+		bot := NewSimple(c.in)
+		got := bot.Solve(NewGrid(c.in))
+		if got != c.out {
+			t.Error("Unexpected output.")
+		}
+	}
+}
+
+func TestSmartSolve(t *testing.T) {
+	size := geometry.NewSize(2, 2)
+	pointA := geometry.NewPoint(1, 1)
+	pointB := geometry.NewPoint(0, 0)
+	points := []*geometry.Point{pointA, pointB}
+	config := &Config{Size: size, Points: points}
+	bot := NewSmart()
+	got := bot.Solve(NewGrid(config))
+	if got != "Smart Solution" {
+		t.Error("Unexpected output.")
+	}
+}
